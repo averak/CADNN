7c98b5aa08c8a551823cfbb123f540b13e25a6e5
AjxLab <abe12@mccc.jp>
2020-05-26 22:02:19 +0900

    nomalize

--- train.py
+++ train.py
@@ -1,6 +1,46 @@
 #!/usr/bin/env python
+import numpy as np
+import librosa
+from sklearn import preprocessing
+import glob
+import tqdm
 import cadnn
 
+
+def feature_extract():
+    #--------------------------------------------
+    # .wavファイルを読み込み
+    #--------------------------------------------
+
+    files = glob.glob('data/context1/*/*.wav')
+    classes = list(set([f.split('/')[-2] for f in files]))
+
+    x = []
+    y = []
+
+    for f in files:
+        wav, fs = librosa.load(f, sr=8000)
+        mfcc = librosa.feature.mfcc(wav, sr=fs, n_mfcc=32).T
+
+        for frame in mfcc:
+            x.append(preprocessing.minmax_scale(frame))
+            y.append(classes.index(f.split('/')[-2]))
+
+    x = np.array(x)
+    y = np.array(y)
+
+    # ランダムに並べ替え
+    perm = np.arange(len(x))
+    np.random.shuffle(perm)
+    x = x[perm]
+    y = y[perm]
+
+    return x, y
+
+
+# 特徴量抽出
+x, y = feature_extract()
+print(x)
 
 # クラス -> [a, i, u, e, o]
 # コンテキスト -> [man, woman]
