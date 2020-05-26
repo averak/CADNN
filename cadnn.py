c26002274e495b8814c697406a9cee505f2fcc38
AjxLab <abe12@mccc.jp>
2020-05-26 21:11:32 +0900

    build simple DNN

---
+++ cadnn.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Dense, Dropout
+
+
+def build_model(n_class, n_context):
+    #--------------------------------------------
+    # モデルをビルド
+    # params:
+    #   - n_class:int -> 分類するクラス数
+    #   - n_context:int -> コンテキスト数
+
+    # メインネットワークの入力層
+    main_input = Input(shape=(32,))
+    # サブネットワークの入力層
+    sub_input = Input(shape=(32,))
+
+    # メインネットワークの中間層
+    main_x1 = Dense(64, activation='relu')(main_input)
+    main_x2 = Dense(64, activation='relu')(main_x1)
+    main_x3 = Dense(64, activation='relu')(main_x2)
+
+    # 出力層
+    y = Dense(n_class, activation='softmax')(main_x3)
+
+    # モデル
+    model = Model(inputs=main_input, outputs=y)
+    return model
+
+
+model = build_model(5, 2)
